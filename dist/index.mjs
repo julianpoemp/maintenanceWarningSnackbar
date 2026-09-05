76518806d3a1c0972e1134aaef31fe8790705daa
Julian Pömp <jp@poemp.net>
2026-09-05 18:41:20 +0200

    update Nx to v22.7.9

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,51 +1,48 @@
-var l = Object.defineProperty;
-var r = (s, n, e) => n in s ? l(s, n, { enumerable: !0, configurable: !0, writable: !0, value: e }) : s[n] = e;
-var a = (s, n, e) => r(s, typeof n != "symbol" ? n + "" : n, e);
-class m {
-  constructor(n) {
-    a(this, "options");
-    this.options = new h(n);
+class l {
+  options;
+  constructor(e) {
+    this.options = new c(e);
   }
   async initialize() {
-    const n = Date.now(), e = await (await fetch(this.options.jsonURL)).json();
+    const e = Date.now(), n = await (await fetch(this.options.jsonURL)).json();
     if (this.options.simulate)
       this.showMaintenanceWarning("This is a test.");
     else
-      for (const t of e)
-        if (t.type === "MAINTENANCE_SCHEDULED") {
-          const o = new Date(t.begin).getTime(), i = n + this.options.nrOfDaysBe4MaintToDisplayMessage * 24 * 60 * 60 * 1e3;
-          if (this.options.verbose && (console.log("------------------"), console.log(n), console.log(o), console.log(i)), o <= i && o >= n) {
-            const c = await fetch(this.options.txtURL);
-            if (c.status === 200) {
-              this.showMaintenanceWarning(await c.text());
+      for (const s of n)
+        if (s.type === "MAINTENANCE_SCHEDULED") {
+          const t = new Date(s.begin).getTime(), a = e + this.options.nrOfDaysBe4MaintToDisplayMessage * 24 * 60 * 60 * 1e3;
+          if (this.options.verbose && (console.log("------------------"), console.log(e), console.log(t), console.log(a)), t <= a && t >= e) {
+            const o = await fetch(this.options.txtURL);
+            if (o.status === 200) {
+              this.showMaintenanceWarning(await o.text());
               break;
             } else
               this.options.verbose && console.log("no maintenance text found!");
           }
         }
   }
-  showMaintenanceWarning(n) {
-    const e = document.createElement("div");
-    e.innerHTML = n, e.id = "maintenance-warning-snackbar", document.body.appendChild(e);
-    const t = document.createElement("button");
-    t.className = "maintenance-warning-snackbar-close-button", t.innerHTML = "X", t.onclick = () => {
-      e.className = e.className.replace("show", "");
-    }, e.appendChild(t), e.className = "show", setTimeout(() => {
-      e.className = e.className.replace("show", "");
+  showMaintenanceWarning(e) {
+    const n = document.createElement("div");
+    n.innerHTML = e, n.id = "maintenance-warning-snackbar", document.body.appendChild(n);
+    const s = document.createElement("button");
+    s.className = "maintenance-warning-snackbar-close-button", s.innerHTML = "X", s.onclick = () => {
+      n.className = n.className.replace("show", "");
+    }, n.appendChild(s), n.className = "show", setTimeout(() => {
+      n.className = n.className.replace("show", "");
     }, 1e4);
   }
 }
-class h {
-  constructor(n) {
-    a(this, "jsonURL");
-    a(this, "txtURL");
-    a(this, "nrOfDaysBe4MaintToDisplayMessage", 3);
-    a(this, "simulate", !1);
-    a(this, "verbose", !1);
-    Object.assign(this, n);
+class c {
+  jsonURL;
+  txtURL;
+  nrOfDaysBe4MaintToDisplayMessage = 3;
+  simulate = !1;
+  verbose = !1;
+  constructor(e) {
+    Object.assign(this, e);
   }
 }
 export {
-  h as MaintenanceWarningOptions,
-  m as MaintenanceWarningSnackbar
+  c as MaintenanceWarningOptions,
+  l as MaintenanceWarningSnackbar
 };
